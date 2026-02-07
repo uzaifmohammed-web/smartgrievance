30e402e264e5337aa7301bf4b2ac6ea9219124b9
uzaifmohammed-web <uzaif.mohammed@universal.edu.in>
2026-02-07 22:41:47 +0530

    Add JWT middleware for token verification

---
+++ backend/middleware/authenticateToken.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+    if (!token) return res.sendStatus(401); // Unauthorized
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403); // Forbidden
+        req.user = user; // Attach user info to request
+        next();
+    });
+};
+
+module.exports = authenticateToken;
